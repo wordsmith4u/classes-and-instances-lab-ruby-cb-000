e12668471d97a9651851b45cb3793878127321b9
Ronald Gerlach <wordsmith4u@hotmail.com>
2018-05-02 15:13:39 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,5 @@
 class Dog
 end
-  fido = dog.new
-  snoopy = dog.new
-  lassie = dog.new
+  fido = Dog.new
+  snoopy = Dog.new
+  lassie = Dog.new
--- lib/person.rb
+++ lib/person.rb
@@ -2,4 +2,3 @@ class Person
 end
   adele_goldberg = Person.new
   alan_kay = Person.new
-
